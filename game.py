e356cff1c881e350431a6674fed97d6c62c5c941
fairiemists <v.deweyness@gmail.com>
2022-07-05 07:33:51 -0700

    Add player creation

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from sunau import AUDIO_FILE_ENCODING_LINEAR_16
 from human import Human
 from ai import AI
 
@@ -9,8 +10,9 @@ class Game:
         self.player_two = None
 
 
-    def welcome():
+    def welcome(self):
         # general rules
+        print()
         print(f"Welcome to Rock, Paper, Scissors, Lizard, Spock.")
         print()
         print(f"Each game will have three rounds.")
@@ -32,29 +34,50 @@ class Game:
         print()
 
 
-    def player_choice():
+    def player_choice(self):
         # 0, 1, 2
+        print()
+        print(f"You can have up to two players. (0, 1, 2)")
         number_of_players = input("How many players? ")
         #create players
-        pass
+        if number_of_players == "0":
+            print(f"You have chosen to have the computer play against itself.")
+            self.player_one = AI("Player One")
+            self.player_two = AI("Player Two")
+        elif number_of_players == "1":
+            print(f"You have chosen to play against the computer.")
+            self.player_one = Human("Player One")
+            self.player_two = AI("Player Two")
+        elif number_of_players == "2":
+            print(f"You have chosen to play against another person.")
+            self.player_one = Human("Player One")
+            self.player_two = Human("Player Two")
+        else: 
+            print("I did not understand that response. Please choose from the following options.")
+            return self.player_choice()
 
-    def choose_gesture():
+
+    def choose_gesture(self):
         #different functions for player 1 & 2? ...I don't think so.
         #pull from Human or AI
-        #print gesture codes
-        #display gesture choices
         #display round winner (or tie)
         #change score
         #next round prompt?
         pass
 
-    def winner_check():
+    def winner_check(self):
         #check for game winner
             #is player wins at least 2 and more than the other player?
                 #YES: Announce winner. Move on to Big Hooray.
                 #NO: Go to gesture_codes then repeat choose_gesture.        # 
         pass
 
-    def big_hooray():
+    def big_hooray(self):
         #offer to play again
         pass
+
+    def run_game(self):
+        print()
+        self.welcome()
+        print()
+        self.player_choice()
--- human.py
+++ human.py
@@ -6,8 +6,29 @@ class Human(Player):
 
 
     def choose_gesture(self): 
+        
+        #display gesture choices
         print()
-        self.current_gesture = input(f"Enter your gesture choice: ")
+        #print gesture codes
+        print(f"Press 1 for {self.gestures[0]}")
+        print(f"Press 2 for {self.gestures[1]}")
+        print(f"Press 3 for {self.gestures[2]}")
+        print(f"Press 4 for {self.gestures[3]}")
+        print(f"Press 5 for {self.gestures[4]}")
+        user_input = input(f"Enter your gesture choice: ")
+        if user_input == "1":
+            self.current_gesture = self.gestures[0]
+        elif user_input == "2":
+            self.current_gesture = self.gestures[1]
+        elif user_input == "3":
+            self.current_gesture = self.gestures[2]
+        elif user_input == "4":
+            self.current_gesture = self.gestures[3]
+        elif user_input == "5":
+            self.current_gesture = self.gestures[4]
+        else:
+            print(f"I didn't understand that. Please choose a gesture from the list.")
+            return self.choose_gesture()
         print(f"{self.name} picked {self.gestures[int(self.current_gesture)]}")
         
     
